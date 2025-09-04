9eb4a4f4f8647c2cb271198b8ed293fa93d43377
DTH235622_PHAM CAM DAO <111565334+Phamcamdao@users.noreply.github.com>
2025-09-04 20:29:18 +0700

    Add files via upload

---
+++ baitap/bai1.py
@@ -0,0 +1,38 @@
+# bai tap chuong 1
+print('chào các bạn')
+print('Tôi là Pham Cam Dao')
+# vi du 1
+print('Hello World!')
+# vi du 2
+print('Albert Einstein đã từng nói: Một người không bao giờ mắc sai lầm không bao giờ thử bất kỳ điều gì mới.')
+# vi du 3
+total = 1 + \
+2 + \
+3
+print(total); print('Hello python')
+# vi du 4
+if True:
+    print('True')
+else:
+    print('False')
+# vi du 5
+#This is comment
+print('Hello, World!')
+# vi du 6
+import math
+import os
+# vi du 7
+def add(a, b):
+    result = a + b
+    return result
+# vi du 8
+import math_libs
+print(math_libs.add(4,5))
+# vi du 9
+import module1
+print(module1.question)
+print(module1.answer)
+# vi du 10
+import module2
+print(module2.question)
+print(module2.answer)
